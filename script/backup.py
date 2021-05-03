03ca297bc4734d00959a3caa42344292047f1cc1
Mugaya Joel Patrick <josean.patrick11@gmail.com>
2021-05-03 15:30:11 +0300

    Update backup.py

--- script/backup.py
+++ script/backup.py
@@ -1,7 +1,7 @@
 
 from datetime import datetime
 import logging
-import subprocess
+import os
 import time
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', filename='/home/cwakibi/mayanapp/backup.log', level=logging.DEBUG)
@@ -9,9 +9,8 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', filename
 try:
     while True:
         thedatetime = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
-        # python2 => subprocess.run(["ls", "-l"])
-        # subprocess.call([f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup-{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}.tar.gz' /docker-volumes/mayan-edms/"]) 
-        result = subprocess.call([f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup-{thedatetime}.tar.gz' ~/joeldelete/"])  # Test
+        # os.system(f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup-{thedatetime}.tar.gz' /docker-volumes/mayan-edms/") 
+        result = os.system(f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup {thedatetime}.tar.gz' /docker-volumes/mayan-edms/")  # Test
         logging.info(result)
         time.sleep(20)
 except Exception as e:
