c9692778b2b41db881f845012bdf63b586b92859
Mugaya Joel Patrick <josean.patrick11@gmail.com>
2021-05-03 14:06:39 +0300

    Create backup.py

---
+++ script/backup.py
@@ -0,0 +1,17 @@
+
+from datetime import datetime
+import logging
+import time
+import subprocess
+
+logging.basicConfig(filename='/home/cwakibi/mayanapp/backup.log', encoding='utf-8', level=logging.DEBUG)
+
+try:
+    while true:
+        # python2 => subprocess.run(["ls", "-l"])
+        # subprocess.call([f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup-{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}.tar.gz' /docker-volumes/mayan-edms/"]) 
+        result = subprocess.call([f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup-{datetime.now().strftime("%m-%d-%Y, %H:%M:%S")}.tar.gz' ~/joeldelete/"])  # Test
+        logging.info(result)
+        time.sleep(20)
+except:
+    logging.error("An Error occurred!")
