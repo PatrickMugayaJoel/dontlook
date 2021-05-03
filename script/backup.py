59cae39aa01170cfd7c3926bf5911245c0be8aaa
Mugaya Joel Patrick <josean.patrick11@gmail.com>
2021-05-03 15:00:57 +0300

    Update backup.py

--- script/backup.py
+++ script/backup.py
@@ -7,7 +7,7 @@ import time
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', filename='/home/cwakibi/mayanapp/backup.log', level=logging.DEBUG)
 
 try:
-    while true:
+    while True:
         thedatetime = datetime.now().strftime("%m-%d-%Y, %H:%M:%S")
         # python2 => subprocess.run(["ls", "-l"])
         # subprocess.call([f"sudo tar -zcvf  '/home/cwakibi/mayanapp/backup-{datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}.tar.gz' /docker-volumes/mayan-edms/"]) 
